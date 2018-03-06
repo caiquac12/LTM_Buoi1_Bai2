1bfea1c6e89bbac5f3ae9a6b60b9a242ce63714f
caiquac12 <caiquac70@gmail.com>
2018-03-06 17:29:39 +0700

    Done

---
+++ LTM_Buoi1_Bai2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace LTM_Buoi1_Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Moi ban nhap ip hoac ten mien : ");
+            string host = Console.ReadLine();
+
+            IPHostEntry entry = Dns.Resolve(host);
+            Console.WriteLine("Hostname : " + entry.HostName);
+            int ip = 0;
+            foreach (IPAddress ipinhost in entry.AddressList)
+                Console.WriteLine(" Ip : " + ipinhost);
+            
+        }
+        }
+    }
